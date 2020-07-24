4900ad334dbbce2fefddec0eb568b3dc8b3299a4
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2020-07-24 21:35:34 +0700

    init: base app structure

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../blocks/header';
+
+const App = () => {
+
+  return (
+    <Fragment >
+      <div>Dream about Pizza</div>
+    </Fragment>  
+  );
+}
+
+export default App;
---
+++ src/components/error-indicator/error-indicator.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+  return <div>Some error!</div>
+};
+
+export default ErrorIndicator;
